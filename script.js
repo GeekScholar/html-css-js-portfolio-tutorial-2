5a9df3df13d73b955cb182ee55e868d7140bf323
Gaurav Yagvalya <gaurav.y.india@gmail.comm>
2023-06-09 04:25:47 +0530

    #adding animation

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+
+const observer = new IntersectionObserver((enteries) => {
+  enteries.forEach((entery) => {
+      console.log(entery)
+      if (entery.isIntersecting) {
+          entery.target.classList.add('show');
+      } else {
+          entery.target.classList.remove('show');
+      }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
